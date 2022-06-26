a1d9392b1f6c37c593280336ea0d381a2d2b7b72
AlinaMrk <alinamarunchak@yahoo.com>
2022-06-26 16:41:01 +0300

    goit-js-hw-09

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,50 +1,39 @@
 import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
 import Notiflix from 'notiflix';
+import 'flatpickr/dist/flatpickr.min.css';
 
-const onDateInput = document.querySelector('#datetime-picker');
-const start = document.querySelector('[data-start]');
-const days = document.querySelector('[data-days]');
-const hours = document.querySelector('[data-hours]');
-const minutes = document.querySelector('[data-minutes]');
-const seconds = document.querySelector('[data-seconds]');
+const inputEl = document.querySelector('#datetime-picker')
+const startBtn = document.querySelector('button[data-start]')
+const daysOutput = document.querySelector('[data-days]')
+const hoursOutput = document.querySelector('[data-hours]')
+const minutesOutput = document.querySelector('[data-minutes]')
+const secondsOutput = document.querySelector('[data-seconds]')
 
-start.disabled = true;
+startBtn.disabled = 'disabled'
+
+let timerId = null
 
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
     onClose(selectedDates) {
-      if (selectedDates[0] <= new Date()) {
-          start.disabled = true;
-          return Notiflix.Notify.failure('Please choose a date in the future'); 
+      console.log(selectedDates[0]);
+      if (this.selectedDates[0] > Date.now()) {
+        startBtn.disabled = null
+      } else {
+        return Notiflix.Notify.failure('Please choose a date in the future');
       }
-      Notiflix.Notify.success('Great, lets start timing');
-        console.log(selectedDates[0]);
-      start.disabled = false;   
-      
-      start.addEventListener('click', timerOn);
-        
-      function timerOn(e) {         
-        setInterval(() => {
-          let timeDifference = selectedDates[0] - new Date();
+    },
+}
 
-          if (timeDifference > 0) {    
-            addLeadingZero();
-            
-            days.textContent = `${convertMs(timeDifference).days}`;
-            hours.textContent = `${convertMs(timeDifference).hours}`;
-            minutes.textContent = `${convertMs(timeDifference).minutes}`;
-            seconds.textContent = `${convertMs(timeDifference).seconds}`;
-          }
-        }, 1000);
-        };
-    },     
-};
+const fp = flatpickr(inputEl, options)
 
-flatpickr(onDateInput, options);
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0')
+}
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -53,17 +42,42 @@ function convertMs(ms) {
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
-  const days = Math.floor(ms / day);
-  // Remaining hours
-  const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = addLeadingZero(Math.floor(ms / day));
 
-  return { days, hours, minutes, seconds };
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+
+  const mins = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+
+  const secs = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+  return { days, hours, mins, secs };
 }
-function addLeadingZero(value){
-    return String(value).padStart(2, '0');
-}
+
+const timer = {
+  start() {
+      let selectedTime = fp.selectedDates[0].getTime();
+      let currentTime = Date.now()
+      timerId = setInterval(() => {
+        
+      currentTime = Date.now()
+      const deltaTime = selectedTime - currentTime
+      const { days, hours, mins, secs } = convertMs(deltaTime)
+
+      daysOutput.textContent = days
+      hoursOutput.textContent = hours
+      minutesOutput.textContent = mins
+      secondsOutput.textContent = secs
+      // let sum = (Number(days) + Number(hours) + Number(mins) + Number(secs))
+      
+        if (deltaTime <= 1000) {
+          clearInterval(timerId)
+        }
+
+      }, 1000)
+      
+    
+    startBtn.disabled = 'disabled'
+    } 
+  }
+
+startBtn.addEventListener('click', timer.start)
